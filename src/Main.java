a1e1b64c77ef5b5f2772da1a0451d0aef3b0365d
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-08 09:18:33 -0300

    Ultimo commit do exercicio. Terminando, arrumando e ajustando alguns detalhes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Util util = new Util();
-        util.mainMenu();
+        new Util().menuPrincipal();
     }
 }
--- src/Util.java
+++ src/Util.java
@@ -1,14 +1,16 @@
+import java.text.DecimalFormat;
+
 import static javax.swing.JOptionPane.*;
 import static java.lang.Integer.parseInt;
 import static java.lang.Double.parseDouble;
 
 public class Util {
     private Produto produto[] = new Produto[3];
-    private Fornecedor provider[] = new Fornecedor[3];
-    private int indexProduct = 0;
-    private int indexProvider = 0;
+    private Fornecedor fornecedor[] = new Fornecedor[3];
+    private int indexProduto = 0;
+    private int indexFornecedor = 0;
 
-    public void mainMenu() {
+    public void menuPrincipal() {
         String menu = "1- Cadastrar produto\n2- Pesquisar produto por nome\n3- Pesquisar fornecedor por CNPJ\n 4- Finalizar";
         int option;
 
@@ -17,16 +19,15 @@ public class Util {
             if (option == 4) {
                 return;
             }
-
             switch (option) {
                 case 1:
-                    addProduct();
+                    adicionarProduto();
                     break;
                 case 2:
-                    searchName();
+                    procurarProduto();
                     break;
                 case 3:
-                    searchProvider();
+                    pesquisar();
                     break;
                 default:
                     showMessageDialog(null, "Opção inválida!");
@@ -34,44 +35,67 @@ public class Util {
         }
     }
 
-    private void addProduct() {
-        String nome;
-        double value;
-        int qtdEstoque;
-        Fornecedor provider = searchProvider();
-
-        if(provider == null) {
-            provider = addProvider();
+    private void pesquisar() {
+        Fornecedor fornecedor = procurarFornecedor();
+        if(fornecedor != null) {
+            String aux = "";
+            aux+= "Fornecedor: " + fornecedor.getNome() + "\n";
+            aux+= "CNPJ: " + fornecedor.getCnpj() + "\n";
+            showMessageDialog(null, aux);
         }
-
-        nome = showInputDialog("Nome do produto");
-        value = parseDouble(showInputDialog("Valor unitário"));
-        qtdEstoque = parseInt(showInputDialog("Quantidade em estoque"));
-        produto[indexProduct] = new Produto(nome, value, qtdEstoque, provider);
     }
 
-    private Fornecedor addProvider() {
+    private void adicionarProduto() {
+        String nome;
+        double valor;
+        int qtdEstoque;
+        Fornecedor fornecedor = procurarFornecedor();
+
+        if(fornecedor == null) {
+            fornecedor = adicionarFornecedor();
+        }
+
+        nome = showInputDialog("Nome do produto: ");
+        valor = parseDouble(showInputDialog("Valor unitário: "));
+        qtdEstoque = parseInt(showInputDialog("Quantidade em estoque: "));
+        produto[indexProduto] = new Produto(nome, valor, qtdEstoque, fornecedor);
+        indexProduto++;
+    }
+
+    private Fornecedor adicionarFornecedor() {
         Fornecedor fornecedor;
         String nome = showInputDialog("Nome do fornecedor");
         int cnpj =  parseInt(showInputDialog("CNPJ"));
         fornecedor =  new Fornecedor(nome, cnpj);
-        this.provider[indexProvider] = fornecedor;
-        indexProvider++;
+        this.fornecedor[indexFornecedor] = fornecedor;
+        indexFornecedor++;
 
         return fornecedor;
     }
 
-    private void searchName() {
-    }
-
-    private Fornecedor searchProvider() {
-        int cnpj = parseInt(showInputDialog("CNPJ do fornecedor"));
-        for (int i = 0; i< indexProvider; i++) {
-            if(provider[i].getCnpj() == cnpj ) {
-                return provider[i];
+    private void procurarProduto() {
+        DecimalFormat df = new DecimalFormat("R$0,00");
+        String nome = showInputDialog("Nome do produto: ");
+        String aux = "Produto não encontrado!";
+        for(int i = 0; i< indexProduto; i++) {
+            if(produto[i].getProduto().equalsIgnoreCase(nome)){
+                aux = "";
+                aux += "Nome do produto: "+nome+"\n";
+                aux += "Valor unitário: " + df.format(produto[i].getValor()) + "\n";
+                aux +="Nome do fornecedor: " + produto[i].getFornecedor().getNome() + "\n";
             }
         }
-        showMessageDialog(null, +cnpj+" não encontrado");
+        showMessageDialog(null, aux);
+    }
+
+    private Fornecedor procurarFornecedor() {
+        int cnpj = parseInt(showInputDialog("CNPJ do fornecedor"));
+        for (int i = 0; i< indexFornecedor; i++) {
+            if(fornecedor[i].getCnpj() == cnpj ) {
+                return fornecedor[i];
+            }
+        }
+        showMessageDialog(null, +cnpj+" não encontrado!");
         return null;
     }
 }
